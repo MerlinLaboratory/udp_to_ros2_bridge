26b18abeda2395a741d14744afff6ba7483bec67
galileo <marco.faroni@polimi.it>
2025-08-25 15:05:46 +0200

    galileo : tested on real hw

--- launch/udp_to_ros2_bridge_launch.py
+++ launch/udp_to_ros2_bridge_launch.py
@@ -1,13 +1,32 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
+
+    pkg_dir = get_package_share_directory('udp_to_ros2_bridge')
+
+    config_path_cmd = DeclareLaunchArgument(
+        'config_path',
+        default_value=pkg_dir + '/config/comm_param.yaml',
+        description='Full path to the config file')
+
+    node = Node(
             package='udp_to_ros2_bridge',               # Replace with your package name
             executable='udp_to_ros2_bridge',       # Node executable
             name='udp_to_ros2_bridge',                     # ROS 2 node name
-            parameters=['config/comm_param.yaml'],      # YAML file path
-            output='screen'                             # Print logs to terminal
-        )
-    ])
+            parameters=[
+                LaunchConfiguration('config_path'),
+        ])
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    ld.add_action(config_path_cmd)
+    ld.add_action(node)
+    return ld
